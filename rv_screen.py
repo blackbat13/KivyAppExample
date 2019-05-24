8e70dff8cee9778be9907f9629fe4b060826454c
blackbat13 <blackbat13@gmail.com>
2019-05-24 16:18:48 +0200

    Modified population of recycle view

--- rv_screen.py
+++ rv_screen.py
@@ -1,12 +1,20 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
 from random import sample
 from string import ascii_lowercase
 
+
 class RvScreen(Screen):
 
-    def populate_button_pressed(self):
-        self.rv.data = [{'value': ''.join(sample(ascii_lowercase, 6))}
-                        for x in range(50)]
+    def populate(self):
+        data = []
+        for x in range(50):
+            obj = {}
+            obj['value'] = ''.join(sample(ascii_lowercase, 6))
+            obj['id'] = str(x)
+            data.append(obj)
+
+        self.rv.data = data
 
     def login_button_pressed(self):
         username = self.username_text_input.text
@@ -15,3 +23,14 @@ class RvScreen(Screen):
 
     def back_button_pressed(self):
         self.manager.current = 'main_screen'
+
+    def on_enter(self, *args):
+        self.populate()
+
+
+class Row(BoxLayout):
+    def first_button_pressed(self):
+        print(self.id, self.value)
+
+    def second_button_pressed(self):
+        print(self.id, self.value)
