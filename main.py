5d5d06e07c82ed7e995fff0c29d605fae02655cb
blackbat13 <blackbat13@gmail.com>
2019-05-24 17:17:14 +0200

    Added icons

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from kivy.app import App
+from kivymd.theming import ThemeManager
 import login_screen
 import rv_screen
 
 
 class MainApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.theme_cls = ThemeManager()
+        self.theme_cls.primary_palette = 'Blue'
+
     def login_button_pressed(self):
         self.root.current = 'login_screen'
 
